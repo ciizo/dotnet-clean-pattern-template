5984dda99b912ad38463d0e86076338ec6ea008c
chatupon <khochatupon@gmail.com>
2023-12-05 21:37:08 +0700

    add unit-test for GetUserAsync

--- tests/Ciizo.Restful.Onion.UnitTests/Domain/Business/User/UserServiceTests.cs
+++ tests/Ciizo.Restful.Onion.UnitTests/Domain/Business/User/UserServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Banking.Domain.Service.Models;
+using Ciizo.Restful.Onion.Domain.Business.Exceptions;
 using Ciizo.Restful.Onion.Domain.Business.User;
 using Ciizo.Restful.Onion.Domain.Core.Repository;
 using Ciizo.Restful.Onion.UnitTests.TestFakers.User;
@@ -17,16 +18,58 @@ namespace Ciizo.Restful.Onion.UnitTests.Domain.Business.User
         }
 
         [Fact]
-        public async Task CreateUserAsync_InvalidRequestData_ReturnCreatedUserDto()
+        public async Task CreateUserAsync_ValidRequestData_ReturnCreatedUserDto()
         {
             SetUp();
             UserCreateDtoFaker userCreateDtoFaker = new();
-            UserCreateDto dto = userCreateDtoFaker.Generate();
+            UserCreateDto userCreateDto = userCreateDtoFaker.Generate();
             _userRepositoryMock.Setup(x => x.SaveChangesAsync(CancellationToken.None)).Returns(Task.CompletedTask);
 
-            var result = await _userService.CreateUserAsync(dto, CancellationToken.None);
+            var result = await _userService.CreateUserAsync(userCreateDto, CancellationToken.None);
 
-            var expected = new UserDto(result.Id, dto.Email, dto.Name);
+            var expected = new UserDto(result.Id, userCreateDto.Email, userCreateDto.Name);
+            result.Should().BeEquivalentTo(expected);
+        }
+
+
+        [Fact]
+        public async Task GetUserAsync_InvalidId_ThrowArgumentException()
+        {
+            SetUp();
+            Guid invalidId = default;
+
+            var act = async () => await _userService.GetUserAsync(invalidId, CancellationToken.None);
+
+            await act.Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Id is required.");
+        }
+
+        [Fact]
+        public async Task GetUserAsync_IdNotFound_ThrowDataNotFoundException()
+        {
+            SetUp();
+            Guid notExistedId = Guid.NewGuid();
+            _userRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>(), CancellationToken.None))
+                .ReturnsAsync((Onion.Domain.Core.Entities.User)null!);
+
+            var act = async () => await _userService.GetUserAsync(notExistedId, CancellationToken.None);
+
+            await act.Should().ThrowAsync<DataNotFoundException>()
+                .WithMessage("User not found.");
+        }
+
+        [Fact]
+        public async Task GetUserAsync_ValidId_ReturnUserDto()
+        {
+            SetUp();
+            UserFaker userFaker = new();
+            Onion.Domain.Core.Entities.User entity = userFaker.Generate();
+            Guid id = entity.Id;
+            _userRepositoryMock.Setup(x => x.GetByIdAsync(id, CancellationToken.None)).ReturnsAsync(entity);
+
+            var result = await _userService.GetUserAsync(id, CancellationToken.None);
+
+            var expected = new UserDto(id, entity.Email, entity.Name);
             result.Should().BeEquivalentTo(expected);
         }
     }
---
+++ tests/Ciizo.Restful.Onion.UnitTests/TestFakers/User/UserFaker.cs
@@ -0,0 +1,12 @@
+﻿namespace Ciizo.Restful.Onion.UnitTests.TestFakers.User
+{
+    public class UserFaker : Faker<Onion.Domain.Core.Entities.User>
+    {
+        public UserFaker()
+        {
+            RuleFor(x => x.Id, faker => faker.Random.Guid());
+            RuleFor(x => x.Email, faker => faker.Internet.ExampleEmail());
+            RuleFor(x => x.Name, faker => faker.Name.FullName());
+        }
+    }
+}
